8f6395fd6ec9cba468b1ecf644aae1d7aacd4c21
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-12-05 14:24:22 -0500

    Implement javascript for changing SPECIAL stats.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,39 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+$(document).ready(function (){
+  $(".increase-special").on("click", increaseSpecial);
+  $(".decrease-special").on("click", decreaseSpecial);
+});
+
+function increaseSpecial(){
+  //Increment corresponsding SPECIAL stat by 1 point
+  pointsRemaining = parseInt($(".points-remaining")[0].innerHTML);
+  specialValue = $(this).siblings(".special-level")[0].innerHTML;
+  if (specialValue < 10 && pointsRemaining > 0){
+    $(this).siblings(".special-level")[0].innerHTML = parseInt(specialValue) + 1;
+    updateSpecialRemaining(true);
+  }
+}
+
+function decreaseSpecial(){
+  //Increment corresponsding SPECIAL stat by 1 point
+  specialValue = $(this).siblings(".special-level")[0].innerHTML;
+  if (specialValue > 1){
+    $(this).siblings(".special-level")[0].innerHTML = parseInt(specialValue) - 1;
+    updateSpecialRemaining(false);
+  }
+}
+
+function updateSpecialRemaining(pointSpent){
+  pointsRemaining = parseInt($(".points-remaining")[0].innerHTML);
+  if (pointSpent){
+    if (pointsRemaining > 0){
+      $(".points-remaining")[0].innerHTML = pointsRemaining - 1;
+    }
+  }
+  else {
+    $(".points-remaining")[0].innerHTML = pointsRemaining + 1;
+  }
+}
