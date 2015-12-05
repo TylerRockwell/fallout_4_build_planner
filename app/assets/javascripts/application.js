d370485303257501f8a462a3e06e75ed748e3300
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-12-05 14:57:28 -0500

    Implement javascript for changing Perks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,26 +16,30 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
+// Attach javascript actions to relevant DOM elements
 $(document).ready(function (){
   $(".increase-special").on("click", increaseSpecial);
   $(".decrease-special").on("click", decreaseSpecial);
+  $(".increase-perk").on("click", increasePerk);
+  $(".decrease-perk").on("click", decreasePerk);
 });
 
+// SPECIAL Stat Handling
 function increaseSpecial(){
   //Increment corresponsding SPECIAL stat by 1 point
   pointsRemaining = parseInt($(".points-remaining")[0].innerHTML);
-  specialValue = $(this).siblings(".special-level")[0].innerHTML;
+  specialValue = $(this).prev(".special-level")[0].innerHTML;
   if (specialValue < 10 && pointsRemaining > 0){
-    $(this).siblings(".special-level")[0].innerHTML = parseInt(specialValue) + 1;
+    $(this).prev(".special-level")[0].innerHTML = parseInt(specialValue) + 1;
     updateSpecialRemaining(true);
   }
 }
 
 function decreaseSpecial(){
   //Increment corresponsding SPECIAL stat by 1 point
-  specialValue = $(this).siblings(".special-level")[0].innerHTML;
+  specialValue = $(this).next(".special-level")[0].innerHTML;
   if (specialValue > 1){
-    $(this).siblings(".special-level")[0].innerHTML = parseInt(specialValue) - 1;
+    $(this).next(".special-level")[0].innerHTML = parseInt(specialValue) - 1;
     updateSpecialRemaining(false);
   }
 }
@@ -49,5 +53,38 @@ function updateSpecialRemaining(pointSpent){
   }
   else {
     $(".points-remaining")[0].innerHTML = pointsRemaining + 1;
+  }
+}
+
+// Perk Handling
+
+function increasePerk(){
+  console.log($(this).prev(".perk-level")[0])
+  perkValue = $(this).prev(".perk-level")[0].innerHTML.split("/");
+  current = parseInt(perkValue[0]);
+  max =  parseInt(perkValue[1]);
+  if(current < max){
+    $(this).prev(".perk-level")[0].innerHTML = (current+1) + "/" + max
+    updatePerksUsed(true);
+  }
+}
+
+function decreasePerk(){
+  perkValue = $(this).next(".perk-level")[0].innerHTML.split("/");
+  current = parseInt(perkValue[0]);
+  max =  parseInt(perkValue[1]);
+  if(current > 0){
+    $(this).next(".perk-level")[0].innerHTML = (current-1) + "/" + max;
+    updatePerksUsed(false);
+  }
+}
+
+function updatePerksUsed(perkIncreased){
+  perksUsed = parseInt($(".num-perks")[0].innerHTML);
+  if (perkIncreased){
+    $(".num-perks")[0].innerHTML = perksUsed + 1;
+  }
+  else{
+    $(".num-perks")[0].innerHTML = perksUsed - 1;
   }
 }
